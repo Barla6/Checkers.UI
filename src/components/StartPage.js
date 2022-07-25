53678cbf902e1d1a37a7dbf9e00b277028950701
barla6 <barlaliko62@gmail.com>
2022-07-25 17:33:15 +0300

    1) create basic StartPage 2) adding assets

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./StartPage.module.css";
 
 const StartPage = () => {
-    return <div>hello i'm start-page</div>;
+    return (
+        <div className={styles["start-page"]}>
+            {/* <img
+                className={styles["header-img"]}
+                src={require("../assets/header.png")}
+                alt=""
+            ></img> */}
+            <div className={styles["header"]}></div>
+            <div className={styles["bottom-div"]}>
+                <button className={styles["play-button"]}>play</button>
+            </div>
+        </div>
+    );
 };
 
 export default StartPage;
